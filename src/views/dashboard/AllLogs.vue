5b46313f68029f01fd9b88e6cf53a9cc18d8a6ad
fabiankachlock <fabian.kachlock@icloud.com>
2022-06-14 14:45:36 +0200

    feat: add all logs view styling

--- src/views/dashboard/AllLogs.vue
+++ src/views/dashboard/AllLogs.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="log-view" :ref="(r) => (wrapper = r as HTMLDivElement)"></div>
+  <page-layout title="All Logs">
+    <div class="log-view" :ref="(r) => (wrapper = r as HTMLDivElement)"></div>
+    <template #extra>
+      <n-space>
+        <n-space justify="end" align="center" :wrap="false">
+          <n-space align="center" :wrap="false">
+            <n-p>Show Names</n-p>
+            <n-switch></n-switch>
+          </n-space>
+        </n-space>
+      </n-space>
+    </template>
+  </page-layout>
 </template>
 
 <script setup lang="ts">
 import '@log-rush/log-formatter/dist/index.css';
+import PageLayout from '@/components/util/PageLayout.vue';
+import { NSpace, NSwitch, NP } from 'naive-ui';
 import { ref, watch } from 'vue';
 import { LogFormat, LogFormatter, Optimization } from '@log-rush/log-formatter';
 import { useAllLogs } from '@/core/stores/root';
@@ -36,7 +50,7 @@ const appendLog = (data: string) => {
 
 <style>
 .log-view {
-  max-height: 100%;
+  max-height: calc(100vh - 200px);
   height: 100%;
   overflow-y: auto;
 }
